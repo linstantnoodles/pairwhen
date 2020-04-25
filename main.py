d1249e16ae67717bb52eb57d0646e62c0de62a24
Alan <alin@2u.com>
2020-04-25 11:19:05 -0400

    update

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
