e59f5d9449406494b284f78d54683e88f23984f3
Alan <alin@2u.com>
2020-04-26 17:56:07 -0400

    add mysql connector

---
+++ tests/test_main.py
@@ -0,0 +1,17 @@
+import os
+import tempfile
+import pytest
+from app import main
+
+@pytest.fixture
+def client():
+    db_fd, main.app.config['DATABASE'] = tempfile.mkstemp()
+    main.app.config['TESTING'] = True
+
+    with main.app.test_client() as client:
+        with main.app.app_context():
+            main.init_db()
+        yield client
+
+    os.close(db_fd)
+    os.unlink(main.app.config['DATABASE'])
